c48306e4ed785b035c8116f595d2f48d25300c5e
Tochio Herrera <tochio12910@gmail.com>
2023-03-11 22:51:15 -0500

    added the folders needed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import Reacts, { useState } from 'react';
+import Header from './components/Header';
+import About from './components/About';
+import Contact from './components/Contact';
+import Portfolio from './components/Portfolio';
+import Resume from './components/Resume';
+import Footer from './components/Footer';
+import "./App.css";
+
 
 function App() {
+  const [currentTab, setCurrentTab] = useState('about');
+
+  const renderTab = () => {
+    switch (currentTab) {
+      case 'about':
+        return <About />;
+      case 'portfolio':
+        return <Portfolio />;
+      case 'contact':
+        return <Contact />;
+      case 'resume':
+        return <Resume />;
+      default:
+        return <About />;
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <title>Tochio Herrera Portfolio | {currentTab} </title>
+      <Header currentTab={currentTab} 
+      setCurrentTab={setCurrentTab}
+      ></Header>
+      <main>
+        {renderTab()}
+      </main>
+      <Footer />
     </div>
   );
 }
